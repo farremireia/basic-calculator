f878ba10f90fa0f4d67c37921333e87d604fbbe4
Mireia Farre <67146405+farremireia@users.noreply.github.com>
2020-09-05 15:53:37 +0100

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,43 @@
+/*
+A calculator class that can perform simple arithmetic operations on two integers.
+*/
+
+public class Calculator {
+  public Calculator() {
+
+  }
+
+  public int add(int a, int b) {
+    return a + b;
+  }
+
+  public int substract(int a, int b) {
+    return a - b;
+  }
+
+  public int multiply(int a, int b) {
+    return a * b;
+  }
+
+  public int divide(int a, int b) {
+    return a / b;
+  }
+
+  public int modulo(int a, int b) {
+    return a % b;
+  }
+
+  public static void main(String[] args) {
+    Calculator myCalculator = new Calculator();
+
+    System.out.println(myCalculator.add(5, 7));
+
+    System.out.println(myCalculator.substract(45, 11));
+
+    System.out.println(myCalculator.multiply(6, 7));
+
+    System.out.println(myCalculator.divide(10, 5));
+
+    System.out.println(myCalculator.modulo(10, 3));
+  }
+}
